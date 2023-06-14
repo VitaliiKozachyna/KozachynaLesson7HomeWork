c7a59856e098298c325ea74020816d371823ca7f
VitaliiKozachyna <v.kozachyna@gmail.com>
2023-06-14 21:47:03 +0300

    first commit

---
+++ Src/HomeWork14.java
@@ -0,0 +1,12 @@
+public class HomeWork14 {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Person person3 = new Person();
+
+        System.out.println(person1.personInfo("Rob", "Helford", "London", 442076677677L));
+        System.out.println(person2.personInfo("James", "Hetfield", "Los Angeles", 13233322322L));
+        System.out.println(person3.personInfo("Geoff", "Tate", "Stuttgart", 4921155656657L));
+
+    }
+}
---
+++ Src/Person.java
@@ -0,0 +1,5 @@
+public class Person {
+    public String personInfo(String name, String surname, String town, long phoneNumber) {
+        return "Зателефонувати громадянину " + name + " " + surname + " із міста " + town + " можна за номером телефону " + phoneNumber;
+    }
+}
